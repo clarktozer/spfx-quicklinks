7e63ee3a098d5b85b3f4fd8dfc3e6961604eb732
Clark Tozer <clarkwtozer@gmail.com>
2020-02-09 20:14:42 +1100

    continue upgrade

--- src/webparts/quickLinks/components/QuickLinks.tsx
+++ src/webparts/quickLinks/components/QuickLinks.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import styles from './QuickLinks.module.scss';
-import { IQuickLinksProps } from './IQuickLinksProps';
-import { autobind } from '@uifabric/utilities';
-import { LinkType } from '../QuickLinksWebPart';
+import * as React from "react";
+import styles from "./QuickLinks.module.scss";
+import { IQuickLinksProps } from "./IQuickLinksProps";
+import { LinkType } from "../QuickLinksWebPart";
 import { WebPartTitle } from "@pnp/spfx-controls-react/lib/WebPartTitle";
-import Radium from 'radium';
-import * as tinycolor from 'tinycolor2';
+import Radium from "radium";
+import * as tinycolor from "tinycolor2";
 
 @Radium
 export default class QuickLinks extends React.Component<IQuickLinksProps, {}> {
@@ -14,18 +13,18 @@ export default class QuickLinks extends React.Component<IQuickLinksProps, {}> {
     super(props);
   }
 
-  @autobind
-  private createLinkStyle(hoverColor) {
+  private createLinkStyle = hoverColor => {
     return {
       color: this.props.fontColor,
-      ':hover': {
-        color: tinycolor(hoverColor).darken(25).toString()
+      ":hover": {
+        color: tinycolor(hoverColor)
+          .darken(25)
+          .toString()
       }
     };
-  }
+  };
 
-  @autobind
-  public getIcon() {
+  private getIcon = () => {
     let icon = "";
     switch (this.props.type) {
       case LinkType.FILE:
@@ -35,7 +34,7 @@ export default class QuickLinks extends React.Component<IQuickLinksProps, {}> {
         icon = "Link";
     }
     return icon;
-  }
+  };
 
   public render(): React.ReactElement<IQuickLinksProps> {
     this.inlineStyles = {
@@ -46,24 +45,43 @@ export default class QuickLinks extends React.Component<IQuickLinksProps, {}> {
         <div>
           <div className="ms-Grid-row">
             <div className="ms-Grid-col ms-sm12">
-              <WebPartTitle displayMode={this.props.displayMode}
+              <WebPartTitle
+                displayMode={this.props.displayMode}
                 title={this.props.title}
-                updateProperty={this.props.updateProperty} />
-              {
-                this.props.links.map((e, i) => {
-                  let linkProps = {
-                    key: e.key,
-                    href: e.value
-                  };
-                  if (this.props.openInNewTab) {
-                    linkProps["target"] = "_blank";
-                  }
-                  return <div className={styles.linkRow} key={this.props.type + "-link-" + i}>
-                    <i style={{ color: this.props.iconColor }} className={styles.quickLinkIcon + " ms-Icon ms-Icon--" + this.getIcon()} aria-hidden="true"></i>
-                    <a className={styles.link} {...linkProps} style={this.inlineStyles.link}>{e.label}</a>
-                  </div>;
-                })
-              }
+                updateProperty={this.props.updateProperty}
+              />
+              {this.props.links.map((e, i) => {
+                let linkProps = {
+                  key: e.key,
+                  href: e.value
+                };
+                if (this.props.openInNewTab) {
+                  linkProps["target"] = "_blank";
+                }
+                return (
+                  <div
+                    className={styles.linkRow}
+                    key={this.props.type + "-link-" + i}
+                  >
+                    <i
+                      style={{ color: this.props.iconColor }}
+                      className={
+                        styles.quickLinkIcon +
+                        " ms-Icon ms-Icon--" +
+                        this.getIcon()
+                      }
+                      aria-hidden="true"
+                    ></i>
+                    <a
+                      className={styles.link}
+                      {...linkProps}
+                      style={this.inlineStyles.link}
+                    >
+                      {e.label}
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
